b5860264a136a0e42cf941184314f5cafb7371e3
Linus Lee <linus@thesephist.com>
2019-02-19 16:57:56 -0800

    Memoized, recursive fibonacci

--- closures.go
+++ closures.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
+func newSource() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
 
+func main() {
+    fmt.Println("Printing from tow difference sources")
+
+    source1 := newSource()
+    source2 := newSource()
+
+    fmt.Println(source1())
+    fmt.Println(source2())
+
+    fmt.Println(source1())
+    fmt.Println(source2()) 
+
+    fmt.Println(source1())
+    fmt.Println(source2())
 }
