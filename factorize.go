241eb614db28370f8664b0b796ce5d2e8190b3eb
Linus Lee <linus@thesephist.com>
2019-02-21 16:16:03 -0800

    Improvements to factorize.go to accept multiple inputs and display correct multi-digit exponents

--- factorize.go
+++ factorize.go
@@ -4,23 +4,57 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
+
+const ANSI_BLUE string = "\u001b[34m"
+const ANSI_END string = "\033[0m"
 
 type factorList []int
 
-func (factors factorList) String() string {
+func expChar(exp int) string {
+	EXP_CHARS := []string{"⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"}
+
 	s := ""
-	for _, f := range factors {
-		s += strconv.Itoa(f) + " * "
+	for exp > 0 {
+		s = string(EXP_CHARS[exp%10]) + s
+		exp /= 10
 	}
-	return s[:len(s)-3]
+	return s
+}
+
+func (factors factorList) String() string {
+	parts := []string{}
+	last, lastExp := 0, 1
+	for _, f := range factors {
+		if last == f {
+			lastExp++
+		} else {
+			if lastExp == 1 {
+				parts = append(parts, strconv.Itoa(last))
+			} else {
+				parts = append(parts, strconv.Itoa(last)+expChar(lastExp))
+			}
+			last = f
+			lastExp = 1
+		}
+	}
+	if lastExp == 1 {
+		parts = append(parts, strconv.Itoa(last))
+	} else {
+		parts = append(parts, strconv.Itoa(last)+expChar(lastExp))
+	}
+	return strings.Join(parts[1:], " × ")
 }
 
 func primeFactorize(numberToFactorize int) (factors factorList) {
-	n := numberToFactorize
-	k := 2 // first prime to attempt
+	if numberToFactorize <= 2 {
+		return []int{numberToFactorize}
+	}
+	// potential optimization: only check when k is prime
+	n, k := numberToFactorize, 2
 	factors = []int{}
-	for k < numberToFactorize {
+	for k <= numberToFactorize {
 		for n%k == 0 {
 			n /= k
 			factors = append(factors, k)
@@ -35,14 +69,20 @@ func primeFactorize(numberToFactorize int) (factors factorList) {
 }
 
 func main() {
-	input := os.Args[1]
-	numberToFactorize, err := strconv.Atoi(input)
+	inputs := os.Args[1:]
+	for _, input := range inputs {
+		numberToFactorize, err := strconv.Atoi(input)
 
-	if err != nil {
-		fmt.Println(input, "isn't a valid number to factorize!")
-		os.Exit(1)
+		if err != nil {
+			fmt.Println(input, "isn't a valid number to factorize!")
+			os.Exit(1)
+		}
+
+		factors := primeFactorize(numberToFactorize)
+		if len(factors) == 1 {
+			fmt.Printf("%d%s\tis prime%s\n", numberToFactorize, ANSI_BLUE, ANSI_END)
+		} else {
+			fmt.Printf("%d\t%s\n", numberToFactorize, factors)
+		}
 	}
-
-	factors := primeFactorize(numberToFactorize)
-	fmt.Println("Factorized", numberToFactorize, "->", factors)
 }
